07bb9806378b7e9dbfb6557ee29f13ca037d073a
jiucaios <131468956+jiucaios@users.noreply.github.com>
2026-05-05 13:39:32 +0800

    src

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-require("./src/server");
+const app = require("./src/app");
+
+const PORT = process.env.PORT || 3000;
+
+if (process.env.VERCEL) {
+  module.exports = app;
+} else {
+  app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+  });
+}
